958b2153fb5a059e6105bbefcc62a607e071025f
Bienvenu Massamba <boukamass@gmail.com>
2021-01-20 04:16:26 -0600

    Initial commit

---
+++ app.js
@@ -0,0 +1,25 @@
+// Initialize local git repo
+/git init/
+
+//Config git infos
+/git config --global user.name "Bienvenu Massamba"/
+/git config --global user.email "boukamass@gmail.com"/
+
+//Add files to the local staging area
+/git add index.html/
+
+//Add any html files to the staging area
+/git add *.html/
+
+// Add everything 
+/git add ./ 
+
+//Check what is already in the staging area
+/git status/
+
+//remove files from the staging area
+/git rm --cached index.html/
+
+// Remove any html files from the staging area
+/git rm --cached *.html/
+
